2f8a76a9837a5bcc3884ec8cc63e40be015126cc
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-08 11:59:58 -0400

    Created variables, listener and function for clicking the button. Console Logs on click to verify correct

---
+++ main.js
@@ -0,0 +1,20 @@
+/********************
+ * SELECTOR QUERIES *
+ ********************/
+
+const dogButton = document.getElementById('dogButton')
+
+
+/******************
+ * EVENT LISTENER *
+ ******************/
+dogButton.addEventListener("click", buttonClick)
+
+
+/*************
+ * FUNCTIONS *
+ *************/
+
+function buttonClick() {
+    console.log("Button Clicked")
+}
